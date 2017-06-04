84bdcda6d90a66ab3cde7eb89a5a368ba388f875
Keegan <keegan.hines@gmail.com>
2017-06-04 17:37:41 -0400

    update data fetch script

--- fixtures/datasets/fetch_data.sh
+++ fixtures/datasets/fetch_data.sh
@@ -1,4 +1,4 @@
-#!/usr/sh
+#!/bin/sh
 
 curl https://raw.githubusercontent.com/maravento/blackweb/master/blackurls.txt > url_blacklist.txt
 curl https://raw.githubusercontent.com/maravento/blackweb/master/whiteurls.txt > url_whitelist.txt
