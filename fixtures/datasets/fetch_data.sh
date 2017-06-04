5748195eafa1ab7c3a000f0616d194381aa6e76e
Keegan <keegan.hines@gmail.com>
2017-06-04 17:34:00 -0400

    proj setup

---
+++ fixtures/datasets/fetch_data.sh
@@ -0,0 +1,4 @@
+#!/usr/sh
+
+curl https://raw.githubusercontent.com/maravento/blackweb/master/blackurls.txt > url_blacklist.txt
+curl https://raw.githubusercontent.com/maravento/blackweb/master/whiteurls.txt > url_whitelist.txt
