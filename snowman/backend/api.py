d49b0611c2343c48edde60300035a23650ef5b07
Keegan <keegan.hines@gmail.com>
2017-06-11 21:17:59 -0400

    fixing backend api

--- snowman/backend/api.py
+++ snowman/backend/api.py
@@ -1,11 +1,12 @@
 from flask_restful import Resource, Api, reqparse
 from flask import Flask, request
-from keras.models import load_model
+from snowman.model.text_model import TextModel
 
 app = Flask(__name__)
 api = Api(app)
 
-model = load_model("../../fixtures/models/model_0.0.1")
+model = TextModel()
+model.load()
 
 class ModelApi(Resource):
 	def get(self):
@@ -13,9 +14,9 @@ class ModelApi(Resource):
 
 	def put(self):
 		url = request.form['url']
-		score = model.predict_proba(url)
-		return {"input url" : url, 
-			"score" : score}
+		score = model.predict(str(url))[0][0]
+		return {"input query" : url, 
+			"score" : str(score)}
 
 api.add_resource(ModelApi,"/model")
 
