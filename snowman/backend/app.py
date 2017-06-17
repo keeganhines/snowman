e125be37367b5108384bd50ab312cd76798f6c4c
Keegan <keegan.hines@gmail.com>
2017-06-17 09:00:48 -0400

    adjusting port binding

--- snowman/backend/app.py
+++ snowman/backend/app.py
@@ -22,5 +22,5 @@ class ModelApi(Resource):
 api.add_resource(ModelApi,"/model")
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 33507))
+	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
